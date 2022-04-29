1fe0e9029cf0ad652c517932bb7aed29bffc67bb
Sajith P I <94563958+sajithpi@users.noreply.github.com>
2022-04-29 14:41:24 +0530

    updated commision part

--- show_ft_individuals.py
+++ show_ft_individuals.py
@@ -6,20 +6,26 @@ class Show_Individual:
         self.local_session = Session(bind=engine)
     def show_user(self):
         self.show_user = self.local_session.query(Ft_Individual).join(Treepath).filter(Treepath.descendant==15011).all()
+    # count for to identify the number of levels
         self.count = self.local_session.query(Ft_Individual).join(Treepath).filter(Treepath.descendant==15011).count() - 1
         print("Count:",self.count)
-        # for user in self.show_user:
-        #     print("id:",user.id," username:",user.user_name)
+        self.level = 0;
+
         if self.show_user:
             print("Exists")
-            print(self.show_user)
+            # print(self.show_user)
             for user in self.show_user:
+                print("Level\tAncestorID\tAncestor_Name\tCommission")
                 for des in user.tree:
-                    self.commision = self.local_session.query(Commision).filter(Commision.level==self.count).first()
-                    print(self.commision.commission)
-                    self.amount = round(100 * (self.commision.commission / 100),3)
-                    print("level",self.count,"id:",user.id,"name:",user.user_name,"ancestor:",des.ancestor,"commission:",self.amount)
-                    self.count -=  1
+                    if self.level <= self.count:
+                        self.commision = self.local_session.query(Commision).filter(Commision.level==self.level).first()
+                    # Calculating Commision 
+                        self.amount = round(100 * (self.commision.commission / 100),3)
+                
+                        self.des_name = self.local_session.query(Ft_Individual).filter(Ft_Individual.id==des.ancestor).first()
+                       
+                        print(self.level,"\t",des.ancestor,"\t\t",self.des_name.user_name,"\t",self.amount)
+                        self.level +=  1
         else:
             print("Not Exists")
 ob1 = Show_Individual()
